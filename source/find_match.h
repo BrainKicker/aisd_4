94ca12c0d38b8760d6cf1a23e4763ee01fcf0daf
C00LNICK <30545094+C00LNICK@users.noreply.github.com>
2021-12-24 19:43:56 +0300

    Optimized code.

--- source/find_match.h
+++ source/find_match.h
@@ -4,24 +4,14 @@
 #include <iostream>
 #include <cstring>
 
-const uint32_t p = 999;
-const uint32_t M = 10000;
+const uint32_t p = 1001;
 
-uint32_t add(uint32_t a, uint32_t b) {
-    return (a + b) % M;
-}
-uint32_t sub(uint32_t a, uint32_t b) {
-    return (M + a - b) % M;
-}
-uint32_t mul(uint32_t a, uint32_t b) {
-    return (a * b) % M;
-}
 uint32_t pow(uint32_t a, uint32_t n) {
     uint32_t res = 1;
     while (n) {
         if (n & 1)
-            res = mul(res, a);
-        a = mul(a, a);
+            res *= a;
+        a *= a;
         n >>= 1;
     }
     return res;
@@ -40,12 +30,12 @@ void find_match(const char* pattern, const char* text, std::ostream& out = std::
     uint32_t pattern_h = 0;
 
     for (uint32_t i = 0; i < pattern_len; ++i)
-        pattern_h = add(mul(pattern_h, p), pattern[i]);
+        pattern_h = pattern_h * p + pattern[i];
 
     uint32_t cur_h = 0;
 
     for (uint32_t i = 0; i < pattern_len; ++i)
-        cur_h = add(mul(cur_h, p), text[i]);
+        cur_h = cur_h * p + text[i];
 
     for (uint32_t i = 0; i < text_len - pattern_len + 1; ++i) {
         if (cur_h == pattern_h) {
@@ -60,8 +50,8 @@ void find_match(const char* pattern, const char* text, std::ostream& out = std::
                 out << i << ' ';
             }
         }
-        cur_h = sub(cur_h, mul(text[i], p_n));
-        cur_h = add(mul(cur_h, p), text[i+pattern_len]);
+        cur_h -= text[i] * p_n;
+        cur_h = cur_h * p + text[i+pattern_len];
     }
 
     out << std::endl;
